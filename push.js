3e7353968605acbb82307f2e250c59a920a07f6c
wangsongqing <songqing1117@gmail.com>
2015-01-14 00:10:49 +0530

    消息重新推送机制设计

--- push.js
+++ push.js
@@ -29,6 +29,7 @@ function log(msg){
 
 log('SocketIO > listening on port :3000');
 
+
 /* 
  * Desc:
  * 这个逻辑块主要处理来至 PHP 端的消息；
@@ -70,7 +71,6 @@ redis_client.on("message", function(channel, msg){
 
         log(mid+app);
 
-        if (!(message_query[app+"_"+mid])) message_query[app+"_"+mid] = msg;
 
         // 为安卓客户端构造Json 格式的消息
         var info   = '{"mid":"'+mid+'","msg":"'+obj.msg+'"}';
@@ -90,6 +90,10 @@ redis_client.on("message", function(channel, msg){
 
             //点对点消息状态写入缓存操作;
             redis_io.set(app+"_"+mid,'{"status":"sent"}',redis.print)
+
+            // 将消息写到消息队列中; 如成功接收到APP客户端的反馈，则移除；
+            // 后续在定时器中添加，满n 次推送未收到反馈移除该消息的逻辑
+            if (!(message_query[app+"_"+mid])) message_query[app+"_"+mid] = msg;
 
         } else {
             log("user: " + id+" is not online!");
@@ -140,6 +144,7 @@ ioServer.sockets.on('connection', function(socket) {
             if (!clients[app]) clients[app]={};
 
             socket.uid = user_id;
+            socket.app = app;
             clients[app][user_id]=socket;
 
             // 从未注册列表中删除已注册的socket 实例
@@ -171,6 +176,9 @@ ioServer.sockets.on('connection', function(socket) {
             var app      = client_message.app,
                 mid      = client_message.mid;
 
+            // 消息队列中移除已发送的消息
+            delete message_query[app+"_"+mid];
+            // 缓存中更新消息状态
             redis_io.set(app+"_"+mid,'{"status":"recived"}',redis.print)
 
         } catch (error) {
@@ -184,15 +192,53 @@ ioServer.sockets.on('connection', function(socket) {
 
     // 收到APP掉线事件，将 socket 实例列表删除已下线的socket.
     socket.on('disconnect', function() {
-        delete clients[socket.uid];
+        delete clients[socket.app][socket.uid];
         log('Client gone (id=' + socket.uid + ').');
     });
 
 });
 
 /*
+ *
+ *
+ */
+function resend_message_to_client(msg_index) {
+    try{
+
+        log("resend:"+message_query[msg_index]);
+
+        var obj = JSON.parse(message_query[msg_index]);
+        var id     = obj.id,
+            app    = obj.app,
+            mid    = obj.mid;
+
+        if (!id || !app || !mid || !(obj.msg)) {
+            log("paramter is not complete!")
+            return ;
+        }
+
+        // 为安卓客户端构造Json 格式的消息
+        var info   = '{"mid":"'+mid+'","msg":"'+obj.msg+'"}';
+
+        if (clients[app][id]) {
+            clients[app][id].emit('info',info);
+            // counter++
+
+        } else {
+            log("user: " + id+" is not online!");
+
+            // 用户不在线，删除推送消息对象
+            delete message_query[app+"_"+mid];
+
+            // 消息发送失败原因写入缓存;
+            redis_io.set(app+"_"+mid,'{"status":"faild","reason":"not alive"}',redis.print)
+        }
+    } catch(error) {
+    }
+}
+/*
  * Desc:
- * 定时处理逻辑
+ * 定时器：定时要处理的业务逻辑
  * 定时的时间间隔将作为配置项处理
  */
 
@@ -200,11 +246,9 @@ setInterval(function() {
     // 通知未注册用户注册
     for(x in unreg_clients) unreg_clients[x].emit("info",'{"msg":"unreg"}');
     // 未送达消息重发逻辑
+    for(x in message_query) resend_message_to_client(x);
 
-}, 2000);
-
-// timer set for push info to clients
-//setInterval(function() {
 //    var push_list = clients['msd'];
 //    for (x in push_list) push_list[x].emit("info",'{"msg":"this is message from server"}');
-//}, 2000);
+}, 2000);
+
