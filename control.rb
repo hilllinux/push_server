4f7d86229a36e05c43093628d04f01219b8cf31a
wangsongqing <songqing1117@gmail.com>
2015-02-27 23:43:04 +0800

    更新 daemon控制脚本

--- control.rb
+++ control.rb
@@ -1,3 +1,17 @@
 require 'daemons'
 
-Daemons.run('message_schedule.rb')
+#Daemons.run('message.rb')
+
+def custom_show_status(app)
+      # Display the default status information
+    app.default_show_status
+    puts
+    puts "PS information"
+    system("ps -p #{app.pid.pid.to_s}")
+
+    puts
+    puts "Size of log files"
+    system("du -hs /home/wangsongqing/push_server/message_log")
+end
+
+Daemons.run('message.rb', { show_status_callback: :custom_show_status })
