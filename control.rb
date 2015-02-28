bc912b26f075eddf7b9c535e12ded085e693d786
wangsongqing <songqing1117@gmail.com>
2015-02-28 20:19:19 +0800

    更新 control 选项

--- control.rb
+++ control.rb
@@ -1,17 +1,10 @@
+require 'rubygems'
 require 'daemons'
 
-#Daemons.run('message.rb')
+options = {
+  :app_name   => "message_schedule",
+  :backtrace  => true,
+  :monitor    => true,
+}
 
-def custom_show_status(app)
-      # Display the default status information
-    app.default_show_status
-    puts
-    puts "PS information"
-    system("ps -p #{app.pid.pid.to_s}")
-
-    puts
-    puts "Size of log files"
-    system("du -hs /home/wangsongqing/push_server/message_log")
-end
-
-Daemons.run('message.rb', { show_status_callback: :custom_show_status })
+Daemons.run('message_schedule.rb', options )
